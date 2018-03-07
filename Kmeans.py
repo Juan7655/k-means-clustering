d1fcd6d8ff7c608368c4f6a3ac9ad02167cbe081
juandavid <juan.7655@hotmail.com>
2018-03-06 22:18:23 -0500

    modified to implement elbow method to find optimal number of clusters

--- Kmeans.py
+++ Kmeans.py
@@ -1,15 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import pandas as pd
 
 
 class Kmeans:
 	map_list = []
 	c = None
+	centroid_diff_list = []
 
 	def __init__(self, k, data):
 		self.k = k
 		self.data = data
 
-	def train(self):
+	def train(self, show_graph=False):
+		now = datetime.datetime.now()
 		if self.c is None:
 			self.define_centroids()
 		self.classify_points()
@@ -24,10 +29,33 @@ class Kmeans:
 			actual_c = self.c.copy()
 			self.classify_points()
 			self.define_centroids()
-			print(self.c)
+			print(str(iterations) + ": " + str(self.c))
 			centroid_diff = self.centroid_diff(actual_c)
-		print(iterations)
-		print(centroid_diff)
+			self.centroid_diff_list.append(centroid_diff)
+		print("Iterations: " + str(iterations))
+		print("Centroid difference: " + str(centroid_diff))
+		print("Execution time: " + str(datetime.datetime.now() - now))
+		temp_map = np.array(self.map_list)
+		sum_distance = 0
+		for i in range(self.k):
+			indices_in_cluster = temp_map[temp_map.T[1] == i].T[0]
+			points_in_cluster = pd.DataFrame(self.data.iloc[indices_in_cluster]).reset_index()
+			points_in_cluster.columns = ['i', 'x', 'y']
+			points_in_cluster.drop(0)
+			for j in range(len(points_in_cluster)):
+				# act_point = points_in_cluster[points_in_cluster.columns[0], points_in_cluster.columns[1]][j]
+				temp_p = np.array([points_in_cluster['x'][j], points_in_cluster['y'][j]])
+				sum_distance += get_distance(temp_p, self.c[i])
+			if show_graph:
+				plt.scatter(points_in_cluster[points_in_cluster.columns[1]],  # x values
+				            points_in_cluster[points_in_cluster.columns[2]],  # y values
+				            zorder=1, s=5)
+		if show_graph:
+			plt.scatter(self.c.T[0], self.c.T[1], s=20, zorder=2, c='r')
+			plt.show()
+			plt.plot(self.centroid_diff_list)
+			plt.show()
+		return sum_distance
 
 	def define_centroids(self):
 		if self.c is None:
@@ -66,4 +94,6 @@ def get_distance(p, q):
 		result = suma[0] ** 0.5
 	except AttributeError:
 		result = np.sum(diff) ** 0.5
+	except TypeError:
+		print("lel")
 	return result
--- Main.py
+++ Main.py
@@ -1,12 +1,30 @@
 import Kmeans
 import pandas as pd
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 
 
 def run():
 	data = pd.read_csv("results.csv")
-	model = Kmeans.Kmeans(2, data)
-	model.train()
-	print(model.c)
+	print("my rank is: " + str(rank))
+	print("my size: " + str(comm.Get_size()))
+
+	# plot_distances(data, max_val=6)
+	model = Kmeans.Kmeans(4, data)
+	model.train(show_graph=True)
+
+
+def plot_distances(data, max_val, min_val=2):
+	distances = []
+	for i in range(min_val, max_val):
+		model = Kmeans.Kmeans(i, data)
+		distances.append(model.train(show_graph=False))
+	plt.plot(distances)
+	plt.show()
 
 
 if __name__ == "__main__":
